b5d93c5cae63a67f973674419c53bd625510d00e
himalie <himalie1@gmail.com>
2016-02-15 15:53:50 +1100

    first commit

---
+++ InventManagement.Client.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InventManagement.Client.Web.Startup))]
+namespace InventManagement.Client.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ InventManagement.Domain/Models/InventoryPart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventManagement.Domain.Models
+{
+    public class InventoryPart
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string PartName { get; set; }
+        [Required]
+        public DateTime ManufDate { get; set; }
+        [Required]
+        public DateTime ExpiryDate { get; set; }
+                
+        public decimal Price { get; set; }
+        
+        [MaxLength(2000)]
+        public string Comments { get; set; }
+    }
+}
